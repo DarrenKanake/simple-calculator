6742b4dc4bb23087686abfaa85d5bf6f740149d0
itskanakedarren <kanakedarren@gmail.com>
2025-08-27 00:56:44 +0300

    Modified my assignment

--- calculator.py
+++ calculator.py
@@ -1,7 +1,23 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-result = num1 + num2
-print("The sum is:", result)
-# This code takes two numbers as input and prints their sum.
-# It is a simple addition program.
-# The code is complete and functional.
+# Basic Calculator
+
+# Ask the user for two numbers
+number1 = float(input("Type the first number: "))
+number2 = float(input("Type the second number: "))
+
+# Ask the user what operation to do
+operation = input("Type + for add, - for subtract, * for multiply, / for divide: ")
+
+# Do the calculation
+if operation == "+":
+    print(number1, "+", number2, "=", number1 + number2)
+elif operation == "-":
+    print(number1, "-", number2, "=", number1 - number2)
+elif operation == "*":
+    print(number1, "*", number2, "=", number1 * number2)
+elif operation == "/":
+    if number2 != 0:
+        print(number1, "/", number2, "=", number1 / number2)
+    else:
+        print("Oops! Cannot divide by zero.")
+else:
+    print("Sorry, that is not a correct operation.")
